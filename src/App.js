222c5af001aab874daff2a422fc9d2149a0de772
Steven Seeber <sseeber@alteryx.com>
2021-05-20 08:32:55 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          blabity bla blaasd
+
         </p>
         <a
           className="App-link"
